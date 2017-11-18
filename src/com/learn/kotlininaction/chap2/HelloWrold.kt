eea485dcbbf133aedb271eaa6cb3c33509707fab
Jerry Li <xiaoroyg@163.com>
2017-11-18 22:41:37 +0800

    Learn functions and variables.

--- src/com/learn/kotlininaction/chap2/HelloWrold.kt
+++ src/com/learn/kotlininaction/chap2/HelloWrold.kt
@@ -3,6 +3,8 @@ package com.learn.kotlininaction.chap2
 fun main(args: Array<String>) {
     println("Hello World!")
     println(max(1, 5))
+    languages.add("Kotlin")
+    helloWorld(languages)
 }
 
 
@@ -15,3 +17,15 @@ fun maxB(firstOne : Int, secondOne : Int) : Int =
 
 fun maxC(firstOne : Int, secondOne : Int) =
         if(firstOne > secondOne) firstOne else secondOne
+
+val question = "The ultimate question of lief, the universe, and  everything."
+val answer = 42
+val answerB : Int = 42
+
+val languages = arrayListOf<String>("Java");
+
+fun helloWorld(names : List<String>){
+    val name : String  = if(names.isNotEmpty()) names[0] else "Kotlin"
+    println("Hello $name!")
+    println("Hello ${if(names.isNotEmpty()) names[0] else "Kotlin"}!")
+}
