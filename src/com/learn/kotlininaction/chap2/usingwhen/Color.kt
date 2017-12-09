84753c4af522bbea54a0d14aa575b8d0db8b23ee
Jerry Li <xiaoroyg@163.com>
2017-12-09 23:59:20 +0800

    Learned when.

--- src/com/learn/kotlininaction/chap2/usingwhen/Color.kt
+++ src/com/learn/kotlininaction/chap2/usingwhen/Color.kt
@@ -1,4 +1,4 @@
-package com.learn.kotlininaction.chap2.choices
+package com.learn.kotlininaction.chap2.usingwhen
 
 enum class Color(val red: Int, val green: Int, val blue: Int) {
     RED(255, 0, 0), ORANGE(255, 165, 0),
@@ -27,6 +27,21 @@ fun getWarmth(color: Color) = when (color) {
     Color.GREEN -> "neutral"
     Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
 }
+
+fun getWarmthNoArgument(color: Color) = when{
+    color == Color.RED || color == Color.ORANGE || color == Color.YELLOW -> "warm"
+    color == Color.GREEN -> "neutral"
+    color == Color.BLUE || color == Color.INDIGO || color == Color.VIOLET -> "cold"
+    else -> "plain"
+}
+
+fun mix(color: Color, anotherColor: Color) =
+        when (setOf(color, anotherColor)) {
+            setOf(Color.RED, Color.YELLOW) -> Color.YELLOW
+            setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+            setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+            else -> throw Exception("Dirty Color")
+        }
 
 fun main(args: Array<String>) {
     val mnemonic = getMnemonic(Color.VIOLET)
---
+++ src/com/learn/kotlininaction/chap2/usingwhen/Expression.kt
@@ -0,0 +1,73 @@
+package com.learn.kotlininaction.chap2.usingwhen
+
+
+interface Expression
+
+class Number(val value: Int) : Expression
+
+class Sum(val left: Expression, val right: Expression) : Expression
+
+fun evalWithJavaStyle(expression: Expression): Int {
+    if (expression is Number) {
+        //not smart cast
+        val number = expression as Number
+        return number.value
+    }
+    if (expression is Sum) {
+        //smart cast
+        return evalWithJavaStyle(expression.left) + evalWithJavaStyle(expression.right)
+    }
+    throw IllegalArgumentException("Unknown Expression")
+}
+
+fun eval(expression: Expression): Int =
+        when (expression) {
+            is Number ->
+                expression.value
+            is Sum ->
+                eval(expression.left) + eval(expression.right)
+            else -> throw IllegalArgumentException("Unknown Expression")
+        }
+
+fun evalBlockWithJavaStyle(expression: Expression): Int {
+    if (expression is Number) {
+        val number = expression as Number
+        number.value
+    }
+    if (expression is Sum) {
+        evalBlockWithJavaStyle(expression.left) + evalBlockWithJavaStyle(expression.right)
+    }
+    throw IllegalArgumentException("Unknown Expression")
+}
+
+fun evalBlock(expression: Expression): Int =
+        when (expression) {
+            is Number -> {
+                println("number ${expression.value}")
+                expression.value
+            }
+            is Sum -> {
+                val left = evalBlock(expression.left)
+                val right: Int = evalBlock(expression.right)
+                println("Sum: $left + $right")
+                left + right
+            }
+            else -> throw IllegalArgumentException("Unknown Expression")
+        }
+
+fun blockBody(numberA: Int, numberB: Int): Int {
+    return numberA + numberB
+}
+
+fun expressionBodyA(numberA: Int, numberB: Int): Int =
+        numberA + numberB
+
+fun expressionBodyB(numberA: Int, numberB: Int) =
+        numberA + numberB
+
+/*
+fun notExpressionBody(numberA: Int, numberB: Int): Int =
+    println()
+    numberA + numberB
+}
+*/
