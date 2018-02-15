6e494be9a6912ac708ef041b0e922495276a93b2
Jerry Li <xiaoroyg@163.com>
2018-02-15 17:57:36 +0800

    Learned nullability collections.

---
+++ src/com/learn/kotlininaction/chap6/nullability/NullabilityCollections.kt
@@ -0,0 +1,56 @@
+package com.learn.kotlininaction.chap6.nullability
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun allKindsOfList(): Unit{
+    val listA: List<Int> = ArrayList<Int>()
+    val listB: List<Int?> = ArrayList<Int?>()
+    val listC: List<Int>? = null
+    val ListD: List<Int?>? = ArrayList<Int?>()
+}
+
+fun readNumbers(reader: BufferedReader): List<Int?>{
+    val result = ArrayList<Int?>()
+    for(line in reader.lineSequence()){
+        try {
+            val number = line.toInt()
+            result.add(number)
+        }catch (exception: NumberFormatException){
+            result.add(null)
+        }
+    }
+    return result
+}
+
+fun validNumbers(numbers: List<Int?>): Unit{
+    var validNumberSum = 0
+    var invalidCount = 0
+    for(number in numbers){
+        if(number != null){
+            validNumberSum += number
+        } else {
+            invalidCount ++
+        }
+    }
+    println("Valid Number Sum:$validNumberSum")
+    println("Invalid Count:$invalidCount")
+}
+
+fun validNumberShort(numbers: List<Int?>){
+    val validNumberList: List<Int> = numbers.filterNotNull()
+    println("Valid Number Sum:${validNumberList.sum()}")
+    println("Invalid Count:${numbers.size - validNumberList.size}")
+
+}
+
+fun runNumbers(){
+    val reader = BufferedReader(StringReader("1\nabc\n42"))
+    validNumbers(readNumbers(reader))
+}
+
+fun main(args: Array<String>) {
+    runNumbers()
+}
+
+
