3c680cf27c9dfc6eccfb422221e660099f475262
Jerry Li <xiaoroyg@163.com>
2018-03-05 22:36:16 +0800

    Learned Default and null values for parameters with function types.

--- src/com/learn/kotlininaction/chap8/higherorderfunction/HigherOrderFunction.kt
+++ src/com/learn/kotlininaction/chap8/higherorderfunction/HigherOrderFunction.kt
@@ -24,11 +24,62 @@ fun runTwoAndThree(){
 
 fun String.filter(predicate: (Char) -> Boolean): String{
     val stringBuilder = StringBuilder()
-    for(index in 0..length -1){
+    for(index in 0 until length){
         val element = get(index)
         if(predicate(element)){
             stringBuilder.append(element)
         }
     }
     return stringBuilder.toString()
+}
+
+fun <T> Collection<T>.joinToString(separator: String = ", ", prefix: String = "", postfix: String = "",
+                                   transform:(T) -> String = {it.toString()}): String{
+    val result = StringBuilder(prefix)
+    for((index, element) in withIndex()){
+        if(index > 0){
+            result.append(separator)
+        }
+        result.append(transform(element))
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun <T> Collection<T>.joinToStringNull(separator: String = ", ", prefix: String = "", postfix: String = "",
+                                   transform:((T) -> String)? = null): String{
+    val result = StringBuilder(prefix)
+    for((index, element) in withIndex()){
+        if(index > 0){
+            result.append(separator)
+        }
+        if(transform != null){
+            result.append(transform(element))
+        } else {
+            result.append(element.toString())
+        }
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun <T> Collection<T>.joinToStringNullFunction(separator: String = ", ", prefix: String = "", postfix: String = "",
+                                       transform:((T) -> String)? = null): String{
+    val result = StringBuilder(prefix)
+    for((index, element) in withIndex()){
+        if(index > 0){
+            result.append(separator)
+        }
+        val temp = transform?.invoke(element) ?: element.toString()
+        result.append(temp)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf("Alpha", "Beta")
+    println(list.joinToString())
+    println(list.joinToString{it.toUpperCase()})
+    println(list.joinToString(transform = {it.toLowerCase()}))
 }
