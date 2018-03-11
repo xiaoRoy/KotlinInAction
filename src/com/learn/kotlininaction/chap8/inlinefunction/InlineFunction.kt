e04cafe7bdf3715420707d6d353f4a1f729691eb
Jerry Li <xiaoroyg@163.com>
2018-03-11 15:02:38 +0800

    Learned inlien function.

--- src/com/learn/kotlininaction/chap8/inlinefunction/InlineFunction.kt
+++ src/com/learn/kotlininaction/chap8/inlinefunction/InlineFunction.kt
@@ -1,6 +1,10 @@
 package com.learn.kotlininaction.chap8.inlinefunction
 
+import java.io.BufferedReader
+import java.io.FileReader
 import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 inline fun <T> lock(lock: Lock, action: () -> T): T{
     lock.lock()
@@ -12,9 +16,9 @@ inline fun <T> lock(lock: Lock, action: () -> T): T{
 }
 
 fun actionToLock(lock: Lock){
-    println("before")
+    println("before lock")
     lock(lock){ println("action")}
-    println("after")
+    println("after lock")
 }
 
 fun howInlineWork(lock: Lock){
@@ -26,5 +30,18 @@ fun howInlineWork(lock: Lock){
         lock.unlock()
     }
     println("after lock")
+    val lock: Lock = ReentrantLock()
+    lock.withLock {  }
+
 }
 
+class LockOwner(val lock: Lock){
+    fun runUnderLock(body: () -> Unit){
+        lock(lock, body)
+    }
+}
+
+fun readFirstLineFromFile(path: String): String{
+    BufferedReader(FileReader(path))
+            .use { bufferedReader -> return bufferedReader.readLine() }
+}
