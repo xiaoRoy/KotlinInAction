d1c8ca6f48e8afb082775dc4a3f7dd5d1b07a11a
Jerry Li <xiaoroyg@163.com>
2018-10-31 22:35:33 +0800

    Learned SAM conversion and how to use it.

--- src/main/kotlin/com/learn/kotlininaction/chap5/lambdaexpression/PassingLambdaToJava.kt
+++ src/main/kotlin/com/learn/kotlininaction/chap5/lambdaexpression/PassingLambdaToJava.kt
@@ -1,10 +1,10 @@
 package com.learn.kotlininaction.chap5.lambdaexpression
 
-import java.util.concurrent.Callable
+import java.util.function.Predicate
 
-fun runTask(){
+fun runTask() {
     UseJavaFunctionalInterface.runTask { -> println("running task") }
-    testRunnable(Runnable{ println()})
+    testRunnable(Runnable { println() })
 }
 
 
@@ -13,32 +13,56 @@ fun runTask(){
 * but if you invoke the method has a functional interface, you need to use
 * SAM constructor to make explicit conversion of lambda to functional interface.
 * */
-fun testRunnable(runnable: Runnable){
+fun testRunnable(runnable: Runnable) {
     runnable.run()
 }
 
-val runnable = Runnable { println("running task")}
+val runnable = Runnable { println("running task") }
 
-fun runTaskEquivalent(){
+fun runTaskEquivalent() {
     UseJavaFunctionalInterface.runTask(runnable)
 }
 
-fun runTaskCaptureOutside(taskName: String){
-    UseJavaFunctionalInterface.runTask{ println("running task $taskName")}
+fun runTaskCaptureOutside(taskName: String) {
+    UseJavaFunctionalInterface.runTask { println("running task $taskName") }
 }
 
-fun runTaskAnonymous(){
-    UseJavaFunctionalInterface.runTask(object : Runnable{
+fun runTaskAnonymous() {
+    UseJavaFunctionalInterface.runTask(object : Runnable {
         override fun run() {
             println("running task")
         }
     })
 }
 
-fun createAllDoneRunnable(): Runnable{
+fun runTaskWithLocalRunnable() {
+    // val runnable: () -> Unit = { println("local Running task") }
+    val runnable = { println("local Running task") }
+    UseJavaFunctionalInterface.runTask(runnable)
+}
+
+fun createAllDoneRunnable(): Runnable {
     //compile error, you can not return a lambda
     //return { -> println("All Done")}
-    return Runnable { -> println("All Done")}
+    return Runnable { println("All Done") }
+}
+
+fun filterLagerThanThree() {
+    val filter = Predicate<Int> { number -> number > 3 }
+    val filterB = Predicate { number: Int -> number > 3 }
+    //if a function literal has only one parameter, its declaration may be omitted (along with the -
+    //>),and its name will be it(kotlin doc p98)
+    val filterC = Predicate<Int> { it > 3 }
+    //if the lambda parameter is unused, you can place an underscore instead of its name(kotlin doc p98)
+    val filterD = Predicate<Int> { _ -> true }
+    //we could omit it if we don't use it. but not recommended
+    val filterE = Predicate<Int> { true }
+
+    UseJavaFunctionalInterface.filer(12, filter)
+    UseJavaFunctionalInterface.filer(11, filterB)
+    UseJavaFunctionalInterface.filer(11) {
+        true
+    }
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/learn/kotlininaction/chap5/lambdaexpression/UseJavaFunctionalInterface.java
+++ src/main/kotlin/com/learn/kotlininaction/chap5/lambdaexpression/UseJavaFunctionalInterface.java
@@ -1,10 +1,16 @@
 package com.learn.kotlininaction.chap5.lambdaexpression;
 
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
+
+import java.util.function.Predicate;
 
 public class UseJavaFunctionalInterface {
     public static void runTask(Runnable task){
         task.run();
+    }
+
+    public static <T> void filer(T t, Predicate<T> predicate) {
+        if(predicate.test(t)){
+            System.out.println("matched");
+        }
     }
 }
