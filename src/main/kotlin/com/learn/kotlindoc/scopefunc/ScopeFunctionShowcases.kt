2ae930437061241e106c8dbacc85754fa037756f
Jerry Li <xiaoroyg@163.com>
2019-04-13 18:40:25 +0800

    Learned contexnt object and return value in scope function.

--- src/main/kotlin/com/learn/kotlindoc/scopefunc/ScopeFunctionShowcases.kt
+++ src/main/kotlin/com/learn/kotlindoc/scopefunc/ScopeFunctionShowcases.kt
@@ -1,5 +1,7 @@
 package com.learn.kotlindoc.scopefunc
 
+import kotlin.random.Random
+
 private class Person(
         val name: String,
         var age: Int = 0,
@@ -83,3 +85,57 @@ private val adam = Person("adam").apply {
     age = 11
     moveTo("Beijing")
 }
+
+
+/*
+* hey can be included into call chains as side steps:
+* you can continue chaining function calls on the same object after them.
+* */
+private fun generateAlphabet(): List<String> {
+    val alphabet = mutableListOf<String>()
+    alphabet.also { println("populating the list") }
+            .apply {
+                add("a")
+                add("z")
+                add("w")
+                add("i")
+            }
+            .also { println("sort the list") }
+            .sort()
+    return alphabet
+}
+
+/*
+* They also can be used in return statements of functions returning the context object.
+* */
+private fun getRandomInt(): Int {
+    return Random.nextInt(100).also {
+        print("$it value is generated")
+    }
+}
+
+/*
+* you can use them when assigning the result to a variable, chaining operations on the result
+* */
+private fun countNumberEndWithE(): Int {
+    val numbers = mutableListOf("one", "two", "three")
+    return numbers.run {
+        add("four")
+        add("five")
+        count { it.endsWith("E") }
+    }
+}
+
+/*
+*  you can ignore the return value and use a scope function to create a temporary scope for variables
+* */
+
+private fun showFirstAndLast() {
+    val numbers = listOf(1, 2, 3, 4, 5, 44)
+    with(numbers) {
+        val first = numbers.first()
+        val last = numbers.last()
+        println("First item:$first, last item:$last")
+    }
+}
+
