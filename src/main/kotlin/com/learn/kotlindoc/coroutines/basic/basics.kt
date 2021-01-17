7c1d9265f3f142f6909fd9a28bea0211f02c238f
Jerry Li <xiaoroyg@163.com>
2021-01-17 23:20:24 +0800

    Briding blocking and non-blocking worlds.

--- src/main/kotlin/com/learn/kotlindoc/coroutines/basic/basics.kt
+++ src/main/kotlin/com/learn/kotlindoc/coroutines/basic/basics.kt
@@ -3,15 +3,39 @@ package com.learn.kotlindoc.coroutines.basic
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    first()
+//    first()
+//    second()
+    third()
 }
 
 private fun first() {
     GlobalScope.launch {
-        delay(1000)
-        println("world")
+        delay(1000L)
+        println("World!")
     }
+    println("Hello,")
+    Thread.sleep(2000L)
+}
 
+private fun second() {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    runBlocking {
+        delay(2000L)
+    }
+}
+
+private fun third() = runBlocking {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    delay(2000L)
 }
