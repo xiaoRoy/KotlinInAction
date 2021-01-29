f9c27b81cec1f708947fdf5d589605db66d22837
Jerry Li <xiaoroyg@163.com>
2021-01-30 00:39:27 +0800

    Learned more about coroutines.

--- src/main/kotlin/com/learn/tutorial/coroutine/chap3/Jobs.kt
+++ src/main/kotlin/com/learn/tutorial/coroutine/chap3/Jobs.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.*
 
 fun main() {
 //    first()
-    learnJoin()
+//    learnJoin()
+//    learnJobHierarchy()
+    learnRetry()
 }
 
 fun first() = runBlocking {
@@ -32,5 +34,55 @@ fun learnJoin() {
     }
     runBlocking {
         delay(1000L)
+    }
+}
+
+fun learnJobHierarchy() {
+    with(GlobalScope) {
+        val parentJob = launch {
+            delay(200)
+            println("I am the parent.")
+            delay(200)
+        }
+        launch(context = parentJob) {
+            delay(200)
+            println("I am a child.")
+            delay(200)
+        }
+
+        if (parentJob.children.iterator().hasNext()) {
+            println("The job has children ${parentJob.children}.")
+        } else {
+            println("The job has no children.")
+        }
+    }
+    runBlocking {
+        delay(3000)
+    }
+}
+
+fun learnRetry() {
+    var isDoorOpen = false
+    println("Unlocking the door... please wait.\n")
+
+    GlobalScope.launch {
+        delay(3000)
+        isDoorOpen = true
+    }
+
+    GlobalScope.launch {
+        repeat(4) {
+            println("Trying to open the door...\n")
+            delay(800)
+            if(isDoorOpen) {
+                println("Opened the door")
+            } else {
+                println("The door is still locked\n")
+            }
+        }
+    }
+
+    runBlocking {
+        delay(5000L)
     }
 }
