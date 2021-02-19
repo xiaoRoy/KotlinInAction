f47dab708bc92c6a4f3c56878338320828cf7912
Jerry Li <xiaoroyg@163.com>
2021-02-19 23:20:45 +0800

    Learned composing suspending functions

--- src/main/kotlin/com/learn/kotlindoc/coroutines/basic/ComposingSuspendingFunctions.kt
+++ src/main/kotlin/com/learn/kotlindoc/coroutines/basic/ComposingSuspendingFunctions.kt
@@ -1,15 +1,20 @@
 package com.learn.kotlindoc.coroutines.basic
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import java.lang.IllegalArgumentException
 import kotlin.system.measureTimeMillis
 
 
 fun main() {
 //    defaultSequential()
-    concurrentUsingAsync()
+//    concurrentUsingAsync()
+//    lazilyStartedAsync()
+//    asyncStyle()
+//    asyncStyleWithError()
+//    sumWithStructured()
+//    failedConcurrentSum()
+//    failedConcurrentSumNoScopeBuilder()
+    stillRunning()
 }
 
 
@@ -64,5 +69,123 @@ private fun concurrentUsingAsync() {
         println("Completed in $result")
     }
 }
+
+private fun lazilyStartedAsync() {
+    runBlocking {
+        val time = measureTimeMillis {
+            val one = async(start = CoroutineStart.LAZY) { getFirstCount() }
+            val two = async(start = CoroutineStart.LAZY) { getSecondCount() }
+            one.start()
+            two.start()
+            println("The answer is ${one.await() + two.await()}")
+        }
+        println("Completed in $time")
+    }
+}
+
+private fun asyncStyle() {
+    fun getOneAsync() = GlobalScope.async { getFirstCount() }
+    fun getSecondAsync() = GlobalScope.async { getSecondCount() }
+
+    val time = measureTimeMillis {
+        val one = getOneAsync()
+        val two = getSecondAsync()
+        runBlocking {
+            println("The answer is ${one.await() + two.await()}")
+        }
+    }
+    println("Completed in $time")
+}
+
+private fun somethingWrongBetweenAsyncAndAwait(): Int {
+    throw IllegalArgumentException()
+}
+
+private fun stillRunning() {
+    try {
+        asyncStyleWithError()
+    } catch (exception: IllegalArgumentException) {
+        println("Still running")
+    }
+    runBlocking {
+        delay(10_000L)
+    }
+}
+
+private fun asyncStyleWithError() {
+    fun getOneAsync() = GlobalScope.async {
+        delay(5000L)
+        println("First one is still running!")
+        12
+    }
+
+    fun getSecondAsync() = GlobalScope.async { getSecondCount() }
+
+    val time = measureTimeMillis {
+        val one = getOneAsync()
+        somethingWrongBetweenAsyncAndAwait()
+        val two = getSecondAsync()
+        runBlocking {
+            println("The answer is ${one.await() + two.await()}")
+        }
+    }
+    println("Completed in $time")
+}
+
+private suspend fun concurrentSum(): Int = coroutineScope {
+    val one = async { getFirstCount() }
+    val two = async { getSecondCount() }
+    one.await() + two.await()
+}
+
+
+private fun sumWithStructured() = runBlocking {
+    val time = measureTimeMillis {
+        println("The answer is ${concurrentSum()}")
+    }
+    println("Completed in $time")
+}
+
+
+private val getSumResultWithError: suspend CoroutineScope.() -> Int = {
+    val scope = this
+    val one = scope.async {
+        try {
+            delay(5000L)
+            42
+        } finally {
+            println("First child was cancelled")
+        }
+    }
+    val two = scope.async<Int> {
+        println("Second child throws an exception")
+        throw ArithmeticException()
+    }
+    one.await() + two.await()
+}
+
+private fun failedConcurrentSum() {
+    runBlocking {
+        try {
+            coroutineScope(block = getSumResultWithError)
+        } catch (exception: ArithmeticException) {
+            println("Computation failed with ArithmeticException")
+        }
+    }
+}
+
+private fun failedConcurrentSumNoScopeBuilder() {
+    runBlocking {
+        try {
+            val scope = this
+            scope.getSumResultWithError()
+        } catch (exception: ArithmeticException) {
+            println("Computation failed with ArithmeticException")
+        }
+    }
+}
+
+
+
 
 
