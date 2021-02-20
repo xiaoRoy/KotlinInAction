16cbb32af70e858a55f9facd57e20c96f3e8b88b
Jerry Li <xiaoroyg@163.com>
2021-02-20 23:32:06 +0800

    Learned dispatcher.

---
+++ src/main/kotlin/com/learn/kotlindoc/coroutines/ContextAndDispatcher.kt
@@ -0,0 +1,129 @@
+package com.learn.kotlindoc.coroutines
+
+import com.learn.tutorial.coroutine.showCurrentThreadName
+import kotlinx.coroutines.*
+
+
+fun main() {
+//    showDispatchers()
+//    learnUnconfinedDispatcher()
+//    jumpingBetweenThreads()
+//    notCancelled()
+//    cancelled()
+    cancelledWithContext()
+}
+
+@ObsoleteCoroutinesApi
+private fun showDispatchers() = runBlocking {
+    launch {
+        showCurrentThreadName()
+        println()
+    }
+
+    launch(context = Dispatchers.Unconfined) {
+        println("Dispatchers.Unconfined")
+        showCurrentThreadName()
+        println()
+    }
+
+    launch(context = Dispatchers.Default) {
+        println("Dispatchers.Default")
+        showCurrentThreadName()
+        println()
+    }
+
+    launch(context = newSingleThreadContext("My own thread")) {
+        println("My own thread")
+        showCurrentThreadName()
+        println()
+    }
+}
+
+private fun showThreadMessage(message: String) {
+    println("$message ${Thread.currentThread().name}")
+}
+
+private fun learnUnconfinedDispatcher() = runBlocking {
+    launch(context = Dispatchers.Unconfined) {
+        showThreadMessage("Unconfined:Before first suspension in thread")
+        delay(500L)
+        showThreadMessage("Unconfined:After first suspension in thread")
+    }
+
+    launch {
+        showThreadMessage("main runBlocking:Before first suspension in thread,")
+        delay(1000L)
+        showThreadMessage("main runBlocking:After first suspension in thread,")
+    }
+}
+
+/*
+* jumping between threads
+* */
+
+private fun log(message: String) {
+    println("[${Thread.currentThread().name}] $message")
+}
+
+private fun jumpingBetweenThreads() {
+    runBlocking() {
+        log("runBlocking in main")
+        val scope = this
+        withContext(Dispatchers.IO) {
+            log("withContext in IO")
+        }
+        log("Back in main")
+    }
+}
+
+/*
+* Job in the context
+* */
+private fun notCancelled() = runBlocking {
+    val mainJob = launch {
+        GlobalScope.launch {
+            delay(5000L)
+            println("I am still running!")
+        }
+    }
+    delay(1000L)
+    mainJob.cancel()
+    delay(10_000L)
+}
+
+private fun cancelled() = runBlocking {
+    val outerScope = this
+    val mainJob = outerScope.launch {
+        val innerScope = this
+        val childJob = innerScope.launch {
+            delay(5000L)
+            println("I am still running!")
+        }
+        println("Child Job: ${childJob.hashCode()}")
+    }
+
+    delay(1000L)
+    mainJob.children.forEach {
+        println("Outside child Job: ${it.hashCode()}")
+    }
+    mainJob.cancel()
+    delay(10_000L)
+}
+
+private fun cancelledWithContext() = runBlocking {
+    val outerScope = this
+    val mainJob = outerScope.launch {
+        val innerScope = this
+        withContext(context = Dispatchers.IO) {
+            delay(5000L)
+            println("I am still running!")
+        }
+    }
+
+    delay(1000L)
+    mainJob.children.forEach {
+        println("Outside child Job: ${it.hashCode()}")
+    }
+    mainJob.cancel()
+    delay(10_000L)
+}
