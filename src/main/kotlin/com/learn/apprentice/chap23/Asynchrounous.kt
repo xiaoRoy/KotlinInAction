d080400bf82ed251114dbd75a06d882ce7fccbcb
Jerry Li <xiaoroyg@163.com>
2021-03-10 00:34:05 +0800

    Start coroutine.

--- src/main/kotlin/com/learn/apprentice/chap23/Asynchrounous.kt
+++ src/main/kotlin/com/learn/apprentice/chap23/Asynchrounous.kt
@@ -1,21 +1,36 @@
 package com.learn.apprentice.chap23
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 
 fun main() {
-    startThread()
+//    startThread()
+    startCoroutine()
+}
+
+private val showMessage = { index: Int ->
+    val message = "Message $index from the ${Thread.currentThread().name}"
+    println(message)
 }
 
 private fun startThread() {
-    val showMessage = { index: Int ->
-        val message = "Message $index from the ${Thread.currentThread().name}"
-        println(message)
-    }
+
 
     thread(start = true, name = "not a main thread") {
-        (0..10).forEach (showMessage)
+        (0..10).forEach(showMessage)
     }
 
     (0..10).forEach(showMessage)
 }
+
+private fun startCoroutine() = runBlocking {
+    launch(context = Dispatchers.Default) {
+        (0..10).forEach(showMessage)
+    }
+    (0..10).forEach(showMessage)
+}
+
+
