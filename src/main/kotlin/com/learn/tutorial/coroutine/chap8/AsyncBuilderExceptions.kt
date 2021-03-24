f9469d194c5d8b50c76b9d54e5e55dc8a9b8038b
Jerry Li <xiaoroyg@163.com>
2021-03-24 23:58:47 +0800

    Learned handle multiple exceptions.

--- src/main/kotlin/com/learn/tutorial/coroutine/chap8/AsyncBuilderExceptions.kt
+++ src/main/kotlin/com/learn/tutorial/coroutine/chap8/AsyncBuilderExceptions.kt
@@ -1,11 +1,10 @@
 package com.learn.tutorial.coroutine.chap8
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
-    handleExceptionInAsync()
+//    handleExceptionInAsync()
+    handleMultipleException()
 }
 
 private fun handleExceptionInAsync() = runBlocking {
@@ -20,4 +19,34 @@ private fun handleExceptionInAsync() = runBlocking {
     } catch (exception: IllegalArgumentException) {
         println("Catch $exception")
     }
+}
+
+private fun handleMultipleException() = runBlocking {
+    val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        val message = "Caught $exception with suppressed ${exception.suppressed?.contentToString()}"
+        println(message)
+    }
+
+    val parentJob = GlobalScope.launch(context = coroutineExceptionHandler) {
+        val parentScope = this
+
+        //first child job
+        val parentJob = parentScope.launch {
+            try {
+                delay(1000 * 12)
+            } catch (exception: Exception) {
+                println("$exception caught in first child job")
+            } finally {
+                throw IndexOutOfBoundsException("throw by fainlly")
+            }
+        }
+
+        //second child job
+        parentScope.launch {
+            delay(500)
+            emptyList<Int>().first()
+        }
+        delay(1000 * 20)
+    }
+    parentJob.join()
 }
