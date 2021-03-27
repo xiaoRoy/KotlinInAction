1ae2dd6fd1fd3dce7fa25f44e26a774010514b40
Jerry Li <xiaoroyg@163.com>
2021-03-27 22:23:32 +0800

    Learned cancellation.

---
+++ src/main/kotlin/com/learn/tutorial/coroutine/chap9/Cancellation.kt
@@ -0,0 +1,20 @@
+package com.learn.tutorial.coroutine.chap9
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking {
+    val job = this.launch {
+        repeat(1000) {
+            println("$it. Crunching numbers [Beep.Boop.Beep]")
+            delay(500)
+        }
+    }
+    delay(1300L)
+    println("main: I am tired of waiting!")
+    job.cancel()
+    job.join()
+    println("main: Not I can quit.")
+}
