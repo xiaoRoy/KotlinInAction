2af744c0081a87059e89fb3c8a4d9d7a6ec50c0f
Jerry Li <xiaoroyg@163.com>
2021-05-11 22:22:08 +0800

    Handle async exception.

--- src/main/kotlin/com/learn/kotlindoc/coroutines/exceptions/Exceptions.kt
+++ src/main/kotlin/com/learn/kotlindoc/coroutines/exceptions/Exceptions.kt
@@ -1,13 +1,12 @@
 package com.learn.kotlindoc.coroutines.exceptions
 
 import com.learn.tutorial.coroutine.showCurrentThreadName
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
 
-    demonstrateExceptionFromLaunch()
+//    demonstrateExceptionFromLaunch()
+    demonstrateAsyncException()
 }
 
 
@@ -24,4 +23,28 @@ private fun demonstrateExceptionFromLaunch() = runBlocking {
     }
 
     job.join()
+}
+
+private fun demonstrateAsyncException() = runBlocking {
+    val one: Deferred<Int> = async {
+        delay(10000)
+        throw IndexOutOfBoundsException()
+    }
+    try {
+        val result = one.await()
+    } catch (indexOutOfBoundsException: IndexOutOfBoundsException) {
+        println("can not catch")
+    }
+}
+
+private suspend fun doSomething(scope: CoroutineScope) {
+    val one: Deferred<Int> = scope.async {
+        delay(10000)
+        throw IndexOutOfBoundsException()
+    }
+    try {
+        one.await()
+    } catch (indexOutOfBoundsException: IndexOutOfBoundsException) {
+        println("can not catch")
+    }
 }
