7563de035b9e2c119439d0f45a5f94bf961bcf03
Jerry Li <xiaoroyg@163.com>
2021-05-21 22:43:26 +0800

    Learned coroutineScope.

--- src/main/kotlin/com/learn/article/coroutines/parent/ExceptionInAsyncAgain.kt
+++ src/main/kotlin/com/learn/article/coroutines/parent/ExceptionInAsyncAgain.kt
@@ -1,17 +1,19 @@
 package com.learn.article.coroutines.parent
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.lang.IndexOutOfBoundsException
 import kotlin.system.measureTimeMillis
 
 
 fun main() {
 //    doTwoTaskParallel()
-    while (true){
-        doTwoTaskParallelInRunBlocking()
+    while (true) {
+//        doTwoTaskParallelInRunBlocking()
+//        runGlobalAsync()
+//        runGlobalAsyncHandleError()
+//        errorInCoroutineScope()
+//        runConcurrentSumWithErrorHandleInside()
+        errorInCoroutineScopeHandleOutside()
     }
 }
 
@@ -40,6 +42,68 @@ private suspend fun doSecondTask(): Int {
     delay(1200)
     return 24
 }
+/*
+* exception in second task, using coroutineScope
+* */
+
+private fun errorInCoroutineScope() = runBlocking {
+    concurrentSumWithError()
+}
+
+private suspend fun concurrentSumWithError(): Int {
+    return coroutineScope {
+        val one = async { doFirstTask() }
+        val two = async { doSecondTaskWithError() }
+        one.await() + two.await()
+    }
+}
+
+/*
+* exception in second task, using coroutineScope,
+* handle the exception inside CoroutineScope,
+* still CRASH
+* */
+private suspend fun concurrentSumWithErrorHandleInside(): Int {
+    return coroutineScope {
+        val first = async { doFirstTask() }
+        val second = async { doSecondTaskWithError() }
+        val firstResult = try {
+            first.await()
+        } catch (exception: Exception) {
+            println(exception)
+            0
+        }
+        val secondResult = try {
+            second.await()
+        } catch (exception: Exception) {
+            println(exception)
+            0
+        }
+        firstResult + secondResult
+    }
+}
+
+private fun runConcurrentSumWithErrorHandleInside() = runBlocking {
+    val result = concurrentSumWithErrorHandleInside()
+    println("Result is $result")
+}
+
+/*
+* exception in second task, using coroutineScope,
+* handle the exception outside CoroutineScope,
+* still CRASH
+* */
+
+private fun errorInCoroutineScopeHandleOutside() = runBlocking {
+    val result = try {
+        concurrentSumWithError()
+    } catch (exception: Exception) {
+        println("Caught Exception $exception")
+        0
+    }
+    println("Result is $result")
+}
+
 
 /*
 * exception in second task, not using coroutineScope, in the blockingRun
@@ -69,4 +133,38 @@ private fun doTwoTaskParallelInRunBlocking() = runBlocking {
     }
     val result = firstResult + secondResult
     println("Result is $result")
+}
+
+//global async
+private fun runGlobalAsync() = runBlocking {
+    val first = GlobalScope.async { doFirstTask() }
+    val second = GlobalScope.async { doSecondTaskWithError() }
+    val result = first.await() + second.await()
+    println("Result is: $result")
+}
+
+/*
+* exception in second task, using GlobalScope,
+* not using coroutineScope, try catch the await
+* NO CRASH
+* */
+private fun runGlobalAsyncHandleError() = runBlocking {
+    val first = GlobalScope.async { doFirstTask() }
+    val second = GlobalScope.async { doSecondTaskWithError() }
+
+    val firstResult = try {
+        first.await()
+    } catch (exception: Exception) {
+        println("Catch exception:$exception")
+        0
+    }
+    val secondResult = try {
+        second.await()
+    } catch (exception: Exception) {
+        println("Catch exception:$exception")
+        0
+    }
+    //result is 12
+    val result = firstResult + secondResult
+    println("Result is: $result")
 }
