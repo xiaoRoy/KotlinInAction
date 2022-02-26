885c3aea33dc381bb1e52f7b97523f6090c71655
Jerry Li <xiaoroyg@163.com>
2022-02-26 12:23:43 +0800

    Learned switching dispatcher.

--- src/main/kotlin/com/learn/coroutine/IntroducingCoroutine.kt
+++ src/main/kotlin/com/learn/coroutine/IntroducingCoroutine.kt
@@ -4,19 +4,35 @@ import kotlinx.coroutines.*
 
 
 fun main() {
+    demonstrateDispatcherSwitch()
 
-    GlobalScope.launch(Dispatchers.Main) {
+}
+
+
+
+private fun demonstrateDispatcherSwitch() {
+    showCurrentThreadInfo("main")
+    GlobalScope.launch(Dispatchers.Unconfined) {
+        showCurrentThreadInfo("GlobalScope")
         println("before delay")
         stallForTime()
         println("after delay")
     }
 
     println("immediately")
-
+    runBlocking {
+        delay(3000)
+    }
 }
 
 private suspend fun stallForTime() {
-    withContext(Dispatchers.Default) {
+    withContext(Dispatchers.IO) {
+        showCurrentThreadInfo("stallForTime")
         delay(2000)
     }
+}
+
+fun showCurrentThreadInfo(tag: String) {
+    val currentThreadName = Thread.currentThread().name
+    println("Tag:$tag CurrentThreadName:$currentThreadName")
 }
