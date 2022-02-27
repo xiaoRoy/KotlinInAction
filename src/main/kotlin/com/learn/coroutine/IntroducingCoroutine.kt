602d21d91c77a971673d04f6443366c66150996d
Jerry Li <xiaoroyg@163.com>
2022-02-27 16:11:39 +0800

    Learned coroutine basic builder.

--- src/main/kotlin/com/learn/coroutine/IntroducingCoroutine.kt
+++ src/main/kotlin/com/learn/coroutine/IntroducingCoroutine.kt
@@ -4,10 +4,36 @@ import kotlinx.coroutines.*
 
 
 fun main() {
-    demonstrateDispatcherSwitch()
-
+//    demonstrateDispatcherSwitch()
+//    learnBasicBuilderLaunch()
+    learnAsync()
 }
 
+//The Basic Builder, launch
+
+private fun learnBasicBuilderLaunch() {
+    runBlocking {
+        val scope = this
+        scope.launch {
+            delay(2000)
+            println("This is executed")
+        }
+    }
+}
+
+private fun learnAsync() {
+    runBlocking {
+        val scope = this
+        val deferred = scope.async {
+            delay(2000)
+            println("This is executed after the delay")
+            "not the result"
+        }
+        println("This is called alter calling async()")
+        val result = deferred.await()
+        println("This is the result: $result")
+    }
+}
 
 
 private fun demonstrateDispatcherSwitch() {
