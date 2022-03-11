c791dfcd92fb9e9c6403a1bb22eb385c825bcb61
Jerry Li <xiaoroyg@163.com>
2022-03-11 22:36:59 +0800

    Learned async builder.

--- src/main/kotlin/com/learn/coroutine/CoroutineConcepts.kt
+++ src/main/kotlin/com/learn/coroutine/CoroutineConcepts.kt
@@ -1,14 +1,17 @@
 package com.learn.coroutine
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 /*
 * Programming Android with Kotlin Achieving Structured Concurrency with Coroutines
 * Chapter 7 Coroutines Concepts
 * */
 
+
+fun main() {
+//    firstCoroutine()
+    learnAsyncBuilder()
+}
 
 private fun firstCoroutine() {
     runBlocking {
@@ -24,4 +27,41 @@ private fun firstCoroutine() {
         delay(30)
         job.cancel()
     }
+}
+
+private fun firstCoroutineCantCancel() {
+    runBlocking {
+        val scope = this
+        val job: Job = scope.launch {
+            var index = 0
+            while (true) {
+                println("#$index is working.")
+                index ++
+                Thread.sleep(10)
+            }
+        }
+        delay(30)
+        job.cancel()
+    }
+}
+
+private fun learnAsyncBuilder() = runBlocking {
+    val scope = this
+    val slow = scope.async {
+        var result = 0
+        delay(1000)
+        repeat(1000) {
+            result += it
+        }
+        println("Result is: $result")
+        result
+    }
+
+    val quick = scope.async {
+        delay(100)
+        println("Call complete for quick: 4")
+        5
+    }
+    val result = quick.await() + slow.await()
+    println(result)
 }
