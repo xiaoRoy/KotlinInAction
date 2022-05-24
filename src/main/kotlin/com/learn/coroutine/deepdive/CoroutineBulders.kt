b941d69e98e2ed52e1217a53b84c48010e01f719
Jerry Li <xiaoroyg@163.com>
2022-05-25 01:41:00 +0800

    Structured concurrency

--- src/main/kotlin/com/learn/coroutine/deepdive/CoroutineBulders.kt
+++ src/main/kotlin/com/learn/coroutine/deepdive/CoroutineBulders.kt
@@ -1,9 +1,6 @@
 package com.learn.coroutine.deepdive
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 /*
 *
@@ -11,6 +8,11 @@ import kotlinx.coroutines.runBlocking
 * https://kt.academy/article/cc-builders
 * */
 
+
+fun main() {
+//    learnAsyncBuilder()
+    learnStructureConcurrency()
+}
 
 private fun learnAsyncBuilder() = runBlocking {
     val messageA = GlobalScope.async {
@@ -20,11 +22,51 @@ private fun learnAsyncBuilder() = runBlocking {
 
     val messageB = GlobalScope.async {
         delay(3000L)
-        "Message A"
+        "Message B"
     }
 
     val messageC = GlobalScope.async {
         delay(1000L)
-        "Message A"
+        "Message C"
     }
+    println(messageA.await())
+    println(messageB.await())
+    println(messageC.await())
+}
+
+private fun learnStructureConcurrency()  = runBlocking {
+    val parentScope = this
+    parentScope.launch {
+        delay(4000L)
+        launch {
+            delay(2000L)
+            println("Done!")
+        }
+        println("Last but Second!")
+
+    }
+    parentScope.launch {
+        println("Deeper")
+    }
+    println("Complete?")
+}
+
+/*
+*
+*
+* */
+private fun what() = runBlocking {
+    GlobalScope.launch {
+        delay(1000L)
+        println(" world!")
+    }
+    println("Hello")
+}
+
+private fun where() = runBlocking {
+    launch {
+        delay(1000L)
+        println(" world!")
+    }
+    println("Hello")
 }
